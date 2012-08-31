86680452ce31cf27ef1ac09a79a06a905a5ee733
Dan Lecocq <dan@seomoz.org>
2012-08-31 09:03:36 -0700

    Added backdoor support to the qless-py worker

--- qless/gworker.py
+++ qless/gworker.py
@@ -1,22 +1,32 @@
 #! /usr/bin/env python
 
+'''If you want to use a greenlet-based worker you've come to the right place'''
+
 import os
 import qless
 from qless import worker, logger
 
+
 class Worker(worker.Worker):
+    '''A Greenlet-based Worker'''
     def __init__(self, *args, **kwargs):
         self.pool_size = kwargs.pop('pool_size', 10)
         worker.Worker.__init__(self, *args, **kwargs)
-    
+
     def work(self):
         # We should probably open up our own redis client
         self.client = qless.client(self.host, self.port)
         self.queues = [self.client.queues[q] for q in self.queues]
-        
+
         if not os.path.isdir(self.sandbox):
             os.makedirs(self.sandbox)
-        
+
+        from gevent import backdoor
+        pid = os.getpid()
+        server = backdoor.BackdoorServer(('127.0.0.1', pid))
+        logger.info('Listening on %s' % pid)
+        server.start()
+
         from gevent.pool import Pool
         from gevent import sleep, Greenlet
         pool = Pool(self.pool_size)
@@ -36,9 +46,11 @@ class Worker(worker.Worker):
                         _module = job.klass
                         seen = True
                         pool.start(Greenlet(job.process))
-                
+
                 if not seen:
                     logger.debug('Sleeping for %fs' % self.interval)
                     sleep(self.interval)
             except KeyboardInterrupt:
                 return
+            finally:
+                server.stop()
