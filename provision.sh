ac868dd5785cc1d3c36e818d7e0e76a3e9b44acf
Dan Lecocq <dan@moz.com>
2016-03-20 14:15:24 -0700

    Use pyenv in vagrant.

--- provision.sh
+++ provision.sh
@@ -4,14 +4,34 @@ set -e
 
 # Some dependencies
 sudo apt-get update
-sudo apt-get install -y libhiredis-dev libevent-dev python-pip python-dev
+sudo apt-get install -y git libhiredis-dev libevent-dev python-pip python-dev
 
 # Install redis in support of qless
 sudo apt-get install -y redis-server
 
+# Libraries required to build a complete python with pyenv:
+# https://github.com/yyuu/pyenv/wiki
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
+
+# Install pyenv
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo '
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+' >> ~/.bash_profile
+source ~/.bash_profile
+hash
+
 # Install dependencies and the thing itself
-(
-    cd /vagrant/
-    sudo pip install -r requirements.txt
-    sudo python setup.py install
-)
+pushd /vagrant
+
+    # Install our python version
+    pyenv install
+    pyenv rehash
+
+    # Install our requirements
+    pip install -r requirements.txt
+popd
